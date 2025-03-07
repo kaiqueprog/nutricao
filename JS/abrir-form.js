9d17336fff97487fe9d3495f48026ca060d3f625
kaiqueprog <74382447+kaiqueprog@users.noreply.github.com>
2025-03-07 16:37:11 -0300

    Delete JS directory

--- JS/abrir-form.js
+++
@@ -1,7 +0,0 @@
-// Aparecer o form
-const abrir = document.querySelector('.abrir');
-const form = document.querySelector('form');
-
-abrir.addEventListener('click', () => {
-	form.classList.toggle('ativo');
-});
