8adc2bb2c7fa50d2b9fd98fed8cf86f04fd66d2c
kaiqueprog <74382447+kaiqueprog@users.noreply.github.com>
2025-03-20 14:25:08 -0300

    Delete js directory

--- js/remover-paciente.js
+++
@@ -1,7 +0,0 @@
-const tabelaPaciente = document.getElementById('tabela-pacientes');
-
-tabelaPaciente.addEventListener('dblclick', (e) => {
-		e.target.parentNode.classList.add('esconder');
-		
-		setTimeout(() => {e.target.parentNode.remove();}, 500);
-});
